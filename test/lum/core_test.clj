9c381ece83ecc56ce4670295a72720690807eece
Henrique Marques <allyson2308h@gmail.com>
2024-10-12 17:00:31 +0100

    update lein test

--- test/lum/core_test.clj
+++ test/lum/core_test.clj
@@ -1,7 +1,21 @@
 (ns lum.core-test
   (:require [clojure.test :refer :all]
-            [lum.core :refer :all]))
+            [lum.core :refer :all]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def sample-bookmark-data
+  {:owner "admin"
+   :created-at 1633036800
+   :bookmarks [{:link "https://example.com"
+                :created-at 1633036800
+                :last-updated 1633036800}]})
+
+(deftest test-generate-lum
+  (testing "Generate the bookmark file"
+    (let [home (home-dir)
+          file-path (str home "/bookmarks/lum-marker.json")]
+      (generate-lum)
+      (is (.exists (io/file file-path)))
+      (let [data (-> (slurp file-path) (json/parse-string true))]
+        (is (= (:owner data) "user"))))))
